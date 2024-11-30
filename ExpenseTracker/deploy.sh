a4f25a223220667ae85a06f669c7d8d018009dd8
robertkigobe <robertkigobe@hotmail.com>
2024-11-30 15:01:01 -0500

    Handling http requests

--- ExpenseTracker/deploy.sh
+++ ExpenseTracker/deploy.sh
@@ -7,4 +7,6 @@
 # 2. npx expo install react-native-screens react-native-safe-area-context
 # 3. npm install @react-navigation/native-stack
 # 4. npm install @react-navigation/bottom-tabs
+# 5. create a realtime db in firebase https://console.firebase.google.com/project/react-native-fe957/database/react-native-fe957-default-rtdb/data
+# 6. npm install axios
 
