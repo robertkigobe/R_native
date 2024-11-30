0f13cc3189532f8721a8f9402742cce22f17304a
robertkigobe <robertkigobe@hotmail.com>
2024-11-30 01:37:58 -0500

    Expense tracker start app

---
+++ ExpenseTracker/deploy.sh
@@ -0,0 +1,10 @@
+# expo init ExpenseTracker
+
+# create the screens
+
+# setup navigation 
+# 1. npm install @react-navigation/native
+# 2. npx expo install react-native-screens react-native-safe-area-context
+# 3. npm install @react-navigation/native-stack
+# 4. npm install @react-navigation/bottom-tabs
+
