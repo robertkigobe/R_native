aaf25f52274ac7617bb88b5a4f6a5888c7c123cb
robertkigobe <robertkigobe@hotmail.com>
2024-12-01 20:13:56 -0500

    All apps till Authntication

---
+++ meals/deploy.sh
@@ -0,0 +1,13 @@
+# expo init ExpenseTracker
+
+# create the screens
+
+# setup navigation 
+# 1. npm install @react-navigation/native
+# 2. npx expo install react-native-screens react-native-safe-area-context
+# 3. npm install @react-navigation/native-stack
+# 4. npm install @react-navigation/bottom-tabs
+# 5. create a realtime db in firebase https://console.firebase.google.com/project/react-native-fe957/database/react-native-fe957-default-rtdb/data
+# 6. npm install axios
+# 7. npm install @react-native-async-storage/async-storage
+# 8. expo install expo-app-loading
